6bd703fbf8f9c14ae92889894fedf1e6836b5dd6
AnnaSyr <77345297+AnnaSyr@users.noreply.github.com>
2023-10-10 19:37:34 +0200

    Add Auto class

---
+++ Auto.java
@@ -0,0 +1,8 @@
+public class Auto {
+        public int year;
+        public String brand;
+
+        public Auto(int year, String brand) {
+             this.year = year;
+             this.brand = brand;
+        }
