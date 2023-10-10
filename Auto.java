1f530d8807cd9952571fcb59f6c9dc0954fb591e
Алексей Сырцев <aleksei.turchin@gmail.com>
2023-10-10 20:05:05 +0200

    delete int a

--- Auto.java
+++ Auto.java
@@ -1,10 +1,7 @@
 public class Auto{
     public static void main(String[] args) {
-            public int year;
 
-            public Auto(int year, String ) {
-                this.year = year;
             System.out.println("Hello JAva");
-                           }
+
     }
 }
